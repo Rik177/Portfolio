9e0a06c7b1088b9ff8d944c6880b360164fcc360
Rik177 <mefodi12@gmail.com>
2025-08-24 18:24:56 +0300

    fix images

--- src/components/projectImages.ts
+++
@@ -1,5 +0,0 @@
-import AwesomeKanban from '../images/Awesome_Kanban.png';
-import Nord from '../images/Nord.png';
-import YouTalk from '../images/YouTalk.png';
-
-export { AwesomeKanban, Nord, YouTalk };
